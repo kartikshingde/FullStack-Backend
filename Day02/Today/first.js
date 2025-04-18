b7efb3c1c78bdd90f5945a35377908c4a3acd19c
kartikshingde <kartikshingde12@gmail.com>
2025-04-18 15:21:32 +0530

    cjs vs mjs

---
+++ Day02/Today/first.js
@@ -0,0 +1,13 @@
+// const sum=require("./current/sum")
+// const sub=require("./current/sub")
+// const mul=require("./current/mul")
+const {sum,sub,mul}=require("./calculator")
+
+// sum
+// sub
+// mul
+sum(10,5)
+sub(10,5)
+mul(10,5)
+
+console.log("Hello Ji")
