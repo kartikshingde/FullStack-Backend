40e7339957bdb7556a0af30eaab6d92cb6c87e5d
kartikshingde <kartikshingde12@gmail.com>
2025-07-16 19:21:21 +0530

    database init

---
+++ Namate-Node S2/EP06/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const connectDB = require("./src/config/database");
+const app = express();
+const User = require("./src/models/user");
+
+
+app.post("/signup",async (req,res)=>{
+
+  const user=new User({
+    firstName:"Rohit",
+    lastName:"Sharma",
+    email:"rohit@gmail.com",
+    password:"rohit@321"
+  })
+
+  try{
+    user.save();
+    res.send("Data Added Successfully")
+  }catch(err){
+    res.status(400).send("Some Error Occured")
+  }
+
+})
+
+
+connectDB()
+  .then(() => {
+    console.log("Database connection Established.");
+    app.listen(7777, () => {
+      console.log("App is listening on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.log("Database Connection Failed...");
+  });
