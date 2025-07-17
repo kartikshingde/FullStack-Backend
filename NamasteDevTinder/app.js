11deb0dbff8be54800ee5a90d3eab979b2b55ac3
kartikshingde <kartikshingde12@gmail.com>
2025-07-17 22:30:18 +0530

    update with email, api

--- NamasteDevTinder/app.js
+++ NamasteDevTinder/app.js
@@ -49,38 +49,48 @@ app.get("/feed", async (req, res) => {
 });
 
 //delete user from the database
-app.delete("/user",async (req,res)=>{
-  const UserId=req.body.userId;
-  
+app.delete("/user", async (req, res) => {
+  const UserId = req.body.userId;
 
-  try{
+  try {
     // const user =await User.findByIdAndDelete({_id:userId})
-    const user =await User.findByIdAndDelete(UserId)
-    res.send("User deleted Successfully")
-
-  }catch(err){
-    res.send("Something went wrong !"+err)
+    const user = await User.findByIdAndDelete(UserId);
+    res.send("User deleted Successfully");
+  } catch (err) {
+    res.send("Something went wrong !" + err);
   }
-})
+});
 
 //update data of the user
-app.patch("/user",async (req,res)=>{
-  const userId=req.body.userId;
-  const data=req.body;
-  
-  try{
-    const user=await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after"})
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
     // console.log(user)
-    res.send("User Updated")
-  }catch(err){
-    res.send('Something went wrong'+err)
+    res.send("User Updated");
+  } catch (err) {
+    res.send("Something went wrong" + err);
   }
+});
 
-})
+//update user with emailId
+app.patch("/userByMail", async (req, res) => {
+  const mailId = req.body.mailId;
+  const data = req.body;
+  try {
+    const user = await User.findOneAndUpdate({ email: mailId }, data, {
+      returnDocument: "after",
+    });
 
-
-
-
+    res.send("User Updated Successfully");
+  } catch (err) {
+    res.send("Something Went Wrong -> " + err);
+  }
+});
 
 connectDB()
   .then(() => {
